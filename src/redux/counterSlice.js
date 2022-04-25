4e8d51a4a3d5ff9c6292bde9786c780b44821340
Chimoha31 <mokochii1108@gmail.com>
2022-04-25 16:09:18 -0700

    slice store

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,20 @@
+// Action CreaterとStateとReducerを1つにまとめたものをslicerといい、toolkitを使用し3つ一気に作成する。(pureなreduxでは1つ1つ作っていくしかないがtoolkitのcreateSlice()を使用すれば可能)
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value:0,
+  },
+  // reducerを作成したら、自動的にaction createrも生成される。
+  // increment, decrement etcを実行しますよというaction
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// configureStore()で簡単にstoreの部分を作る。
+export const store = configureStore({
+  reducer: {},
+});
